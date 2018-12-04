afb4290ba8a971d488677e9a537cf8cfe7780fae
haalo <hao20167331@163.com>
2018-12-04 23:11:54 +0800

    import join([timeout]) threading.Lock()

--- day01/helloworld.py
+++ day01/helloworld.py
@@ -1,2 +1,19 @@
-print('Hello World')
-print('{0}'.format('Hello Python'))
+'''
+第一个Python3程序
+'''
+
+
+def first_py_program():
+    string = 'Hello World!'
+    return string
+
+
+class Hello:
+
+    def __init__(self, string='hello world'):
+        self.string = string
+
+    def say(self):
+        print(self.string)
+
+print(__name__)
